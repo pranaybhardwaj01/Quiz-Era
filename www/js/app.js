6b33447d93184893b508919f1dd6a1235cf157c3
Pranay <pranaybhardwaj01@gmail.com>
2017-08-21 20:05:39 +0530

    google API

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,4 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 var starter = angular.module('starter', ['ionic'])
 
 
@@ -15,9 +11,6 @@ starter
             // for form inputs)
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-            // Don't remove this line unless you know what you are doing. It stops the viewport
-            // from snapping when text inputs are focused. Ionic handles this internally for
-            // a much nicer keyboard experience.
             cordova.plugins.Keyboard.disableScroll(true);
         }
         if (window.StatusBar) {
@@ -136,8 +129,6 @@ quizing
                 controller: 'endCtrl',
                 controllerAs: 'end'
             });
-
-        // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/login');
     })
     .controller("quizCtrl", function($firebaseArray, $firebaseAuth, userData, $state) {
